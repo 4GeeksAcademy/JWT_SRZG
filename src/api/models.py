1117ee5c9f3afe75cd24ea40eccb3bec42be19ad
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-05-08 16:57:27 +0000

    se creo modelo favorite y se modifico modelo user

--- src/api/models.py
+++ src/api/models.py
@@ -7,21 +7,45 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(40), nullable=False)
+    dni: Mapped[str] = mapped_column(String(20), nullable=False)
+    nickname: Mapped[str] = mapped_column(String(40), nullable=False)
+    direction: Mapped[str] = mapped_column(String(40), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
     password: Mapped[str] = mapped_column(String(200), nullable=False)
-    fullname: Mapped[str] = mapped_column(String(200), nullable=False)
-    is_active: Mapped[bool] = mapped_column(
-        Boolean(), nullable=False, default=True)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
+    rol: Mapped[str] = mapped_column(String(40), nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "dni": self.dni,
+            "nickname": self.nickname,
+            "direction": self.direction,
             "email": self.email,
-            "fullname": self.fullname,
+            "phone": self.phone,
+            "rol": self.rol
+            
+
             # do not serialize the password, its a security breach
         }
 
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user:  Mapped[str] = mapped_column(String(40), nullable=False)
+    michi: Mapped[str] = mapped_column(String(40), nullable=False)
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "user": self.user,
+            "michi": self.michi
+        }
+        
 
 class TokenBlockedList(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
