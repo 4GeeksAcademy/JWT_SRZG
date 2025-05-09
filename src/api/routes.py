924292024c2cfdcbe1dce5e94afef189bb9a81e4
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-05-09 19:39:07 +0000

    se hizo ruta delete y se creo seccion hasta la linea 200 fede

--- src/api/routes.py
+++ src/api/routes.py
@@ -70,12 +70,131 @@ def user_logout():
     token_blocked = TokenBlockedList(jti=payload["jti"])
     db.session.add(token_blocked)
     db.session.commit()
-    return jsonify({"msg": "User Logged Out"}),200
+    return jsonify({"msg": "User Logged Out"}), 200
 
 
 @api.route("/private", methods=["GET"])
 @jwt_required()
 def private():
-   actually_user = get_jwt_identity()
-   return jsonify(logged_in_as=actually_user),200
+    actually_user = get_jwt_identity()
+    return jsonify(logged_in_as=actually_user), 200
 
+
+# ROUTES FEDE
+
+@api.route("/user/<int:user_id>", methods=["PUT"])
+@jwt_required()
+def update_user(user_id):
+    body = request.get_json()
+    current_user_id = get_jwt_identity()
+    current_user = User.query.get(current_user_id)
+
+    current_user = User(
+        name=current_user['name'],
+        lastname=current_user['lastname'],
+        email=current_user['email'],
+        nickname=current_user['nickname']
+
+    )
+
+    db.session.add(current_user)
+    db.session.commit()
+    return jsonify(current_user.serialize()), 201
+
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": f"User with id {user_id} has been deleted."}), 200
+
+
+""" N
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ """
+# ROUTES FEDE
