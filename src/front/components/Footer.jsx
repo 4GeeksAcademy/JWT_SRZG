bdd74a79f84444aaf2ad87c6a1d09bcf824ab907
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-05-24 17:10:32 +0200

    Footer And Changes

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,7 +1,16 @@
+import michisLogoWt from "../assets/img/michis_wt.png";
+
+
 export const Footer = () => (
-
-	<footer className="footer mt-auto py-3 text-center fixed-bottom">
-		Test
-
+	<footer className="footer d-flex justify-content-center">
+		<div className="justify-content-center">
+			<div className="row text-center">
+				<div className="col-12 p-3"><img className="michis-logo" src={michisLogoWt} /></div>
+				<div className="col-12 p-3">Un sistema para encontrarlos, un sistema atraerlos a todos y adoptarlos en un hogar lleno de Michis. </div>
+			</div>
+			<div className="row mt-auto align-items-end  text-center">
+				<div className="col-12 reserved-rights p-3">All rights reserved 4Michis 2025 &copy;</div>
+			</div>
+		</div>
 	</footer>
 );
