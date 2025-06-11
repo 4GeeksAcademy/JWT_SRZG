9704d2d1c45f68aa89dfdcd2c605ea36f389620d
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-06-11 20:51:32 +0200

    Revert "Iss jaime11"

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -5,7 +5,6 @@ import { RouterProvider } from "react-router-dom"
 import { router } from "./routes"
 import { StoreProvider } from './hooks/useGlobalReducer'
 import { BackendURL } from './components/BackendURL'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Main = () => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
